d024994d5e661b34c64e6399e7caafe444d506e7
Opiti Calvin <opiticalvin@gmail.com>
2021-04-08 16:32:32 +0200

    db creation of roles and synchronization included to server script

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencorded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and Resync DB');
+    initial();
+});
+
 // simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to this Node js Auth application."});
@@ -26,3 +34,20 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
