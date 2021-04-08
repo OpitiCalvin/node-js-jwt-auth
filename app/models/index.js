7788566474cf74929a01e909a5c1e664f2647ffe
Opiti Calvin <opiticalvin@gmail.com>
2021-04-08 19:20:06 +0200

    Sequelize logging disabled

--- app/models/index.js
+++ app/models/index.js
@@ -15,7 +15,10 @@ const sequelize = new Sequelize(
             min: config.pool.min,
             acquire: config.pool.acquire,
             idle: config.pool.idle
-        }
+        },
+
+        // disable logging; default: console.log
+        logging: false
     }
 );
 
